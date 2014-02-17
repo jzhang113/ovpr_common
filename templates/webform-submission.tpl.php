9cf2aaf3c56995e67a20e85fd86900f7925eef83
Pete Lower <pete.lower@gmail.com>
2014-02-17 10:55:54 -0600

    Webform updates on submission

--- templates/webform-submission.tpl.php
+++ templates/webform-submission.tpl.php
@@ -37,9 +37,9 @@
 
 // print '<strong>'.webform_render_children($renderable).'</strong>'; 
 
-$renderable['cid']["#theme"]['#weight']='bold';
-$renderable['node']["#webform"]["#emails"]['#html']='1';
-echo '---cidthemeweight----'.$renderable['cid']["#theme"]['#weight'].'--------';
-echo '---cidthemeweight----'.$renderable['node']["#webform"]["#emails"]['#html'].'--------';
-var_dump($renderable['node']);
+// $renderable['cid']["#theme"]['#weight']='bold';
+// $renderable['node']["#webform"]["#emails"]['#html']='1';
+// echo '---cidthemeweight----'.$renderable['cid']["#theme"]['#weight'].'--------';
+// echo '---cidthemeweight----'.$renderable['node']["#webform"]["#emails"]['#html'].'--------';
+// var_dump($renderable['node']);
 print drupal_render_children($renderable); ?>
