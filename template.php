6935875ecd98a68e51b048ebc73adc7b68cf653b
Brandon Neil <brandon-neil@uiowa.edu>
2015-02-26 14:54:47 -0600

    code formatting cleanup

--- template.php
+++ template.php
@@ -12,7 +12,7 @@
  * used as an alternative to directly editing or adding code to templates. Its
  * worth spending some time to learn more about these functions - they are a
  * powerful way to easily modify the output of any template variable.
- * 
+ *
  * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
  * 1. Rename each function and instance of "adaptivetheme_subtheme" to match
  *    your subthemes name, e.g. if your theme name is "footheme" then the function
@@ -30,7 +30,7 @@ function ovpr_common_preprocess_html(&$vars) {
   global $theme_key;
 
   // Two examples of adding custom classes to the body.
-  
+
   // Add a body class for the active theme name.
   // $vars['classes_array'][] = drupal_html_class($theme_key);
 
@@ -56,7 +56,7 @@ function ovpr_common_process_html(&$vars) {
 function ovpr_common_preprocess_page(&$vars) {
     $departmentname = theme_get_setting('ovpr_depts_name');
     $sitepath = $GLOBALS['base_url'];
-    $vars['ovpr_branding'] = '<a id="research-link" href="http://www.uiowa.edu/">University of Iowa</a>' 
+    $vars['ovpr_branding'] = '<a id="research-link" href="http://www.uiowa.edu/">University of Iowa</a>'
     . l($departmentname, $sitepath, array('attributes' => array('id' => array('current-department-link'))));
     if(theme_get_setting('ovpr_depts_links') === 1) {
     $vars['ovpr_branding'] .=
@@ -67,7 +67,7 @@ function ovpr_common_preprocess_page(&$vars) {
       <li><a title="Human Subjects Office / IRB" href="http://hso.research.uiowa.edu">HSO/IRB</a></li>
       <li><a title="Office of Animal Resources / Institutional Animal Care and USe Committee" href="http://animal.research.uiowa.edu">OAR/IACUC</a></li>
       <li><a title="Research Information Systems" href="http://ris.research.uiowa.edu">RIS</a></li>
-  		<li><a title="Office of the Vice President for Research" href="http://research.uiowa.edu">VPR</a></li>  		
+  		<li><a title="Office of the Vice President for Research" href="http://research.uiowa.edu">VPR</a></li>
   		<li class="last"><a title="See More Units..." href="http://research.uiowa.edu/ovpr-research-units-facilities-and-centers-directory">MORE UNITS</a></li>
   	</ul>';
   	}
