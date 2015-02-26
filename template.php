017c492d3b89e4e833502f1681a22be19d0923ce
Brandon Neil <brandon-neil@uiowa.edu>
2015-02-26 14:55:25 -0600

    rm commented out code

--- template.php
+++ template.php
@@ -21,35 +21,6 @@
  * 2. Uncomment the required function to use.
  */
 
-
-/**
- * Preprocess variables for the html template.
- */
-/* -- Delete this line to enable.
-function ovpr_common_preprocess_html(&$vars) {
-  global $theme_key;
-
-  // Two examples of adding custom classes to the body.
-
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
-  // $vars['classes_array'][] = css_browser_selector();
-
-}
-// */
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function ovpr_common_process_html(&$vars) {
-}
-// */
-
-
 /**
  * Override or insert variables for the page templates.
  */
@@ -72,42 +43,6 @@ function ovpr_common_preprocess_page(&$vars) {
   	</ul>';
   	}
 }
-
-/*function ovpr_common_process_page(&$vars) {
-}
-// */
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function ovpr_common_preprocess_node(&$vars) {
-}
-function ovpr_common_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function ovpr_common_preprocess_comment(&$vars) {
-}
-function ovpr_common_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function ovpr_common_preprocess_block(&$vars) {
-}
-function ovpr_common_process_block(&$vars) {
-}
-// */
 
 /**
  * Return empty string for webform results, if there is no value.
