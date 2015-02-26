0f3629e7db8298fee50e894f88a5f08ec5b1c75a
Brandon Neil <brandon-neil@uiowa.edu>
2015-02-26 14:54:37 -0600

    Fixes #267: animal - Webform empty fields are being rendered in the email

--- template.php
+++ template.php
@@ -108,3 +108,19 @@ function ovpr_common_preprocess_block(&$vars) {
 function ovpr_common_process_block(&$vars) {
 }
 // */
+
+/**
+ * Return empty string for webform results, if there is no value.
+ */
+function ovpr_common_webform_element_text($variables) {
+  $element = $variables['element'];
+  $value = $variables['element']['#children'];
+
+  // Check if there is any value to print out at all.
+  // If not, return an empty string.
+  if (strlen(trim($value)) == 0) {
+    return '';
+  }
+  // Call the default theme function if there is a value.
+  return theme_webform_element_text($variables);
+}
